88e8caf88d080d6d9a0e9ffdf18ef10f408c8666
jarleyprogrammer <jarley.saldarriaga@vecindario.com>
2019-10-01 10:16:34 -0500

    ingresando campos para el usuario y modificacion de la vista de registro y login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_devise_params, if: :devise_controller?
+
+    def configure_devise_params
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:name, :surname, :email, :password, :password_confirmation)
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  #users
   devise_for :users
+  get 'users/sign_in'
+  get 'users/sign_out'
+  get 'users/sign_up'
+  
+  #home
   get 'home/index'
 
   root to: 'home#index'
