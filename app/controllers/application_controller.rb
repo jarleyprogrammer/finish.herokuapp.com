264c6b58dff1584910fb857c5fd6e69564dbfc6b
jarleyprogrammer <jarley.saldarriaga@vecindario.com>
2019-10-02 18:26:30 -0500

    actualizacion de datos de perfil en devise desde aplicaion controller con metodos y key de rails 4.1=>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,13 @@ class ApplicationController < ActionController::Base
     before_action :configure_devise_params, if: :devise_controller?
 
     def configure_devise_params
-        devise_parameter_sanitizer.permit(:sign_up) do |user|
-            user.permit(:name, :surname, :email, :password, :password_confirmation)
-        end
+        attributes = [:name, :surname, :email, :password, :password_confirmation]
+      #  devise_parameter_sanitizer.permit(:sign_up,) do |user|
+            #user.permit(:name, :surname, :email, :password, :password_confirmation)
+        #end
+        devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+        devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+        
     end
 
 end
