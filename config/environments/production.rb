f808daf97f11f845576bacd3e756a52c64a1edee
jarleyprogrammer <jarley.saldarriaga@vecindario.com>
2019-10-02 14:37:16 -0500

    actualizacion de variables para produccion prueba 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
 
   #mandril
   
-  config.action_mailer.default_url_options = { host: 'finish-blog.herokuapp.com' }
-  #config.action_mailer.delivery_method = :smpt
-  #config.action_mailer.smtp_settings = {address: "localhost", port:587 }
+  #config.action_mailer.default_url_options = { host: 'finish-blog.herokuapp.com' }
+  config.action_mailer.delivery_method = :smpt
+  config.action_mailer.smtp_settings = {address: "localhost", port:587 }
   ## TO RESET PASSWORD INSTRUCTION URL
   config.action_mailer.default_url_options = { host: ENV["PASSWORD_RECOVERY_PRIMARY_DOMAIN"] }
   config.action_mailer.smtp_settings = {
